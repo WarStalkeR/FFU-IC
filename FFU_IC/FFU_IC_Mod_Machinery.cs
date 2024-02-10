f5ef1a80fe1b84f60a2b56d00768022974880edc
WarStalkeR <stalker6000@hotmail.com>
2024-02-10 17:48:44 +0200

    Another example update.

--- FFU_IC/FFU_IC_Mod_Machinery.cs
+++ FFU_IC/FFU_IC_Mod_Machinery.cs
@@ -29,14 +29,14 @@ namespace FFU_Industrial_Capacity {
                 "A~>[2][7][7][6][7][7][6][4][2]>'V",
                 "B~>[2][7][7][6][7][7][6][4][2]>'W",
                 "I~>[2][7][7][6][6][6][6][6][2]>~Z",
-                "J#>[2][6][6][6][6][6][6][6][2]>@E"
+                "J@>[2][6][6][6][6][6][6][6][2]>@E"
             }},
         };
         public readonly Dictionary<string, Dictionary<char, char[]>> LayoutMap =
             new Dictionary<string, Dictionary<char, char[]>>() {
             { "ArcFurnaceT1", new Dictionary<char, char[]>() {
                 { 'A', new char[] { 'A', 'B', 'I' }},
-                { 'C', new char[] { 'C', 'D', 'J' }},
+                { 'C', new char[] { 'C', 'D' }},
                 { 'Y', new char[] { 'Y', 'Z' }},
                 { 'V', new char[] { 'V', 'W', 'X' }},
             }},
@@ -108,6 +108,7 @@ namespace FFU_Industrial_Capacity {
             .AddInput(12, Ids.Products.CopperOreCrushed, "*", false)
             .AddInput(2, Ids.Products.Sand, "*", false)
             .AddInput(1, Ids.Products.Graphite, "*", false)
+            .AddInput(1, Ids.Products.Water, "J", false)
             .SetDuration(30.Seconds())
             .AddOutput(16, Ids.Products.MoltenCopper, "*", false, false)
             .AddOutput(6, Ids.Products.Slag, "*", false, false)
