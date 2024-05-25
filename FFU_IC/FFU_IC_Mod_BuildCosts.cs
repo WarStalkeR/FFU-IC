52ee84998beb5febd89e8980cecbae1bc70b2b13
WarStalkeR <stalker6000@hotmail.com>
2024-05-25 23:14:36 +0300

    T4 storages cost modification (increase)

--- FFU_IC/FFU_IC_Mod_BuildCosts.cs
+++ FFU_IC/FFU_IC_Mod_BuildCosts.cs
@@ -2,6 +2,7 @@
 using Mafi.Base;
 using Mafi.Collections.ImmutableCollections;
 using Mafi.Core;
+using Mafi.Core.Buildings.Storages;
 using Mafi.Core.Economy;
 using Mafi.Core.Entities;
 using Mafi.Core.Entities.Static.Layout;
@@ -28,19 +29,23 @@ namespace FFU_Industrial_Capacity {
                 { Ids.Products.ConstructionParts2, 4 },
                 { Ids.Products.Rubber, 4 },
             }},
+            { "StorageT4", new Dictionary<ProductProto.ID, int>() {
+                { Ids.Products.ConstructionParts4, 90 },
+            }},
         };
 
         // Reference Helpers
         private ProductProto PdRef(ProductProto.ID refID) => FFU_IC_IDs.ProductRef(pReg, refID);
         private TransportProto TrnRef(EntityProto.ID refID) => FFU_IC_IDs.TransportRef(pReg, refID);
+        private StorageProto StRef(Mafi.Core.Entities.Static.StaticEntityProto.ID refID) => FFU_IC_IDs.StorageRef(pReg, refID);
 
         // Reflection Helpers
-        public void SetBuildingCost(TransportProto refTransport, Dictionary<ProductProto.ID, int> costData) {
-            if (refTransport == null) { ModLog.Warning($"SetBuildingCost: 'refTransport' is undefined!"); return; }
+        public void SetBuildingCost(EntityProto refEntity, Dictionary<ProductProto.ID, int> costData) {
+            if (refEntity == null) { ModLog.Warning($"SetBuildingCost: 'refEntity' is undefined!"); return; }
             if (costData == null) { ModLog.Warning($"SetBuildingCost: 'costData' is undefined!"); return; }
             if (costData.Count > 4) { ModLog.Warning($"SetBuildingCost: 'costData' beyond 4 not supported!"); return; }
-            string buildCostLog = $"{refTransport.Id} Build Cost:";
-            foreach (var oldProd in refTransport.Costs.Price.Products)
+            string buildCostLog = $"{refEntity.Id} Build Cost:";
+            foreach (var oldProd in refEntity.Costs.Price.Products)
                 buildCostLog += $" {oldProd.Product.Id.ToString().Replace("Product_","")}*{oldProd.Quantity.Value}";
             buildCostLog += " ->";
             foreach (var newProd in costData)
@@ -56,12 +61,12 @@ namespace FFU_Industrial_Capacity {
                 4 => new AssetValue(costList[0], costList[1], costList[2], costList[3]),
                 _ => new AssetValue(),
             };
-            EntityCosts newCosts = new EntityCosts(newBuildCosts, refTransport.Costs.DefaultPriority, 
-                refTransport.Costs.Workers, refTransport.Costs.Maintenance, refTransport.Costs.IsQuickBuildDisabled);
+            EntityCosts newCosts = new EntityCosts(newBuildCosts, refEntity.Costs.DefaultPriority, 
+                refEntity.Costs.Workers, refEntity.Costs.Maintenance, refEntity.Costs.IsQuickBuildDisabled);
             TypeInfo typeCosts = typeof(EntityProto).GetTypeInfo();
             FieldInfo fieldCosts = typeCosts.GetDeclaredField("<Costs>k__BackingField");
             if (fieldCosts != null) {
-                fieldCosts.SetValue(refTransport, newCosts);
+                fieldCosts.SetValue(refEntity, newCosts);
             }
         }
 
@@ -69,17 +74,27 @@ namespace FFU_Industrial_Capacity {
             // Variables Initialization
             pReg = registrator;
 
-            // Transports References
+            // Solid+Loose Belt References
             TransportProto solidT1 = TrnRef(Ids.Transports.FlatConveyor);
             TransportProto solidT2 = TrnRef(Ids.Transports.FlatConveyorT2);
             TransportProto looseT1 = TrnRef(Ids.Transports.LooseMaterialConveyor);
             TransportProto looseT2 = TrnRef(Ids.Transports.LooseMaterialConveyorT2);
 
-            // Transports Modification
+            // High-End Storage References
+            StorageProto refSolidT4 = StRef(Ids.Buildings.StorageUnitT4);
+            StorageProto refLooseT4 = StRef(Ids.Buildings.StorageLooseT4);
+            StorageProto refFluidT4 = StRef(Ids.Buildings.StorageFluidT4);
+
+            // Solid+Loose Belts Cost Modification
             SetBuildingCost(solidT1, buildCostData["ConveyorT1"]);
             SetBuildingCost(solidT2, buildCostData["ConveyorT2"]);
             SetBuildingCost(looseT1, buildCostData["ConveyorT1"]);
             SetBuildingCost(looseT2, buildCostData["ConveyorT2"]);
+
+            // High-End Storages Cost Modification
+            SetBuildingCost(refSolidT4, buildCostData["StorageT4"]);
+            SetBuildingCost(refLooseT4, buildCostData["StorageT4"]);
+            SetBuildingCost(refFluidT4, buildCostData["StorageT4"]);
         }
     }
 }
